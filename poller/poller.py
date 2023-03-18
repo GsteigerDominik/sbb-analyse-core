7f2e21137c5d21424d1bb1897808782a830437d5
GsteigerDominik <dominik.gsteiger@gmail.com>
2023-03-18 16:42:16 +0100

    remove import in poller.py

--- poller/poller.py
+++ poller/poller.py
@@ -1,4 +1,3 @@
-import requests
 print('Hello Scheduler')
 #response = requests.get("https://data.sbb.ch/api/records/1.0/search/?dataset=ist-daten-sbb&q=&facet=betreiber_id&facet=produkt_id&facet=linien_id&facet=linien_text&facet=verkehrsmittel_text&facet=faellt_aus_tf&facet=bpuic&facet=ankunftszeit&facet=an_prognose&facet=an_prognose_status&facet=ab_prognose_status&facet=ankunftsverspatung&facet=abfahrtsverspatung")
 #print(response.json())
