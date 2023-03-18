d417e8d2ac4a3ca4d1a5e5371e4a9711c384f0c7
Lukas Weibel <lukasweibel@air-von-lukas.zhaw.ch>
2023-03-18 17:58:17 +0100

    do it again

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,14 +1,9 @@
 from flask import Flask
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-    )
+app = Flask(__name__)
 
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+@app.route("/")
+def hello_world():
+    return "<p>SBB Analyse!</p>"
 
-    return app
+print("hello world!")
