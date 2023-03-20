3dea28863f251a701820d33e4586b1356e9baaca
GsteigerDominik <dominik.gsteiger@gmail.com>
2023-03-20 21:09:34 +0100

    added config files

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,11 @@
+import configparser as configparser
 from flask import Flask
-
+import configparser
 print('Starting app...')
 app = Flask(__name__)
 from flaskr.api.route import *
 from flaskr.jobs.jobs import *
+
+config = configparser.ConfigParser()
+config.read('flaskr/cfg/'+app.config.get('ENV')+'.ini')
+print(config.getint('APP','test'))
