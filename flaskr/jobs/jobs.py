b315c7448640e007efc6f6cf7861e8e4e808ba04
GsteigerDominik <dominik.gsteiger@gmail.com>
2023-03-29 08:26:00 +0200

    call job on 8:35

--- flaskr/jobs/jobs.py
+++ flaskr/jobs/jobs.py
@@ -5,7 +5,7 @@ import poller.poller
 from flaskr import app
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=poller.poller.runPollJob, trigger='cron', day='*', hour=17, minute=35)
+scheduler.add_job(func=poller.poller.runPollJob, trigger='cron', day='*', hour=8, minute=35)
 scheduler.start()
 # /!\ IMPORTANT /!\ : Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
