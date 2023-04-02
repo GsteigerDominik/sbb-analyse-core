fe6b148b59ad2dec44913db5835101f95fea4695
GsteigerDominik <dominik.gsteiger@gmail.com>
2023-04-02 22:19:53 +0200

    run process job

--- flaskr/jobs/jobs.py
+++ flaskr/jobs/jobs.py
@@ -11,6 +11,6 @@ scheduler = BackgroundScheduler(timezone="Europe/Zurich")
  #TODO maybe make the job able to do multithreading
 scheduler.start()
 scheduler.add_job(func=poller.run_poll_job, trigger='cron', day='*', hour=4, minute=0)
-scheduler.add_job(func=processor.run_process_job, trigger='cron', day='*', hour=22, minute=16)
+scheduler.add_job(func=processor.run_process_job, trigger='cron', day='*', hour=22, minute=21)
 # /!\ IMPORTANT /!\ : Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
