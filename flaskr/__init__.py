19e29a3faebf2389405aa1f7de507d6f58e8346b
GsteigerDominik <dominik.gsteiger@gmail.com>
2023-04-05 16:57:18 +0200

    removed cross site endpoints

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,14 +3,12 @@ from flask import Flask
 
 from flaskr.log import logger
 from flaskr.processor import processor
-from flask_cors import CORS
 
 logger.log_info('Starting app...')
 
 app = Flask(__name__)
 from flaskr.api.route import *
 from flaskr.jobs.jobs import *
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 config = configparser.ConfigParser()
 config.read('flaskr/cfg/' + app.config.get('ENV') + '.ini')
