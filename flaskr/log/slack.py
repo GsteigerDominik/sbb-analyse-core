303d6a1eaa5f1078f335906f37e150ab33de351a
GsteigerDominik <dominik.gsteiger@gmail.com>
2023-04-12 17:11:17 +0200

    added more commands for slack bot

--- flaskr/log/slack.py
+++ flaskr/log/slack.py
@@ -19,14 +19,33 @@ def post_msg(message, channel_id="#sp-arbeit"):
 def handle_event(event):
     message = ""
     if 'help' in event['text']:
-        message = 'You can use following commands:\n' \
-                  'data-status'
-    elif 'data-status' in event["text"]:
-        message = 'We got allot of data :male_mage:\n' \
-                  'For all this dates we got data:'
+        message = ':male_mage: You can use following commands:\n' \
+                  'data-status-raw\n'\
+                  'data-status-traintype\n' \
+                  'data-status-station'
+    elif 'data-status-raw' in event["text"]:
+        message = ':male_mage: We got a lot of raw data\n' \
+                  'For all this dates we got raw data:'
         dates = dbAccess.load_unprocessed_dates()
         for date in dates:
-            message += ' '+ date[0].strftime('%Y-%m-%d')
+            message += ' ' + date[0].strftime('%Y-%m-%d')
+    elif 'data-status-traintype' in event["text"]:
+        message = ':male_mage: We got a lot of processed data\n' \
+                  'For all this dates we got processed data:'
+        dates = dbAccess.load_traintype_delay_dates()
+        for date in dates:
+            message += ' ' + date[0].strftime('%Y-%m-%d')
+    elif 'data-status-station' in event["text"]:
+        message = ':male_mage: We got a lot of processed data\n' \
+                  'For all this dates we got processed data:'
+        dates = dbAccess.load_station_delay_dates()
+        for date in dates:
+            message += ' ' + date[0].strftime('%Y-%m-%d')
     else:
-        message = "Hello, how can i help you? If you dont know me use 'help'!"
+        message = ":male_mage: Hello, how can i help you? If you dont know me, use 'help'!"
     post_msg(message, event["channel"])
+
+
+def post_job_finished_msg(date):
+    message = ':construction_worker: Processed data of ' + date
+    post_msg(message)
