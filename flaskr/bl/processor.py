f0ba5a94b0a22870dbbc11d7e1b655faf2b6856e
Noémie <nobo@bluewin.ch>
2023-05-21 17:37:27 +0200

    h

--- flaskr/bl/processor.py
+++ flaskr/bl/processor.py
@@ -117,14 +117,14 @@ def geometric_distribution_60():
     unique_delays, counts = np.unique(delays, return_counts=True)
     prob = counts / total_delays
 
-    mask = unique_delays <= 30
+    mask = unique_delays <= 60
     unique_delays = unique_delays[mask]
     prob = prob[mask]
 
-    plt.stem(unique_delays, prob, markerfmt='o', use_line_collection=False)
+    plt.plot(unique_delays, prob, markerfmt='o', use_line_collection=False)
     plt.title('Probability of unique Delays')
     plt.xlabel('Delay (minutes)')
     plt.ylabel('Probability')
     plt.grid()
-    plt.xlim(0, 30)
+    plt.xlim(0, 60)
     plt.savefig('./flaskr/static/delaydistribution.png')
